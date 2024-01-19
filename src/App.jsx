81c6a020e72f0032c1cafd71781ed03d363542df
Raman Gupta <gr.gupta.raman@gmail.com>
2024-01-19 14:37:30 -0500

    Add date pickers from shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import { MonthlyRevenueChart } from './components/MonthlyRevenueChart';
 import { OrdersChart } from './components/OrdersChart';
 import SentimentChart from './components/SentimentChart';
+import { DatePicker } from './components/ui/DatePicker';
 import orderData from './dev-data/order_data';
 import pricingData from './dev-data/pricing_data';
 
@@ -11,12 +13,31 @@ const total = orderData
   }, 0);
 
 function App() {
+  const [fromDate, setFromDate] = useState();
+  const [toDate, setToDate] = useState();
+
+  function handleSetFromDate(date) {
+    setFromDate(date);
+    if (!date) setToDate(date);
+    else if (!toDate) setToDate(date);
+    else if (toDate < date) setToDate(date);
+  }
+
+  function handleSetToDate(date) {
+    setToDate(date);
+    if (!date) setFromDate(date);
+    else if (!fromDate) setFromDate(date);
+    else if (date < fromDate) setFromDate(date);
+  }
+
   return (
     <div>
       <h1 className="text-center text-5xl font-bold text-red-500">
         Hello world
       </h1>
 
+      <DatePicker date={fromDate} onSetDate={handleSetFromDate} />
+      <DatePicker date={toDate} onSetDate={handleSetToDate} />
       <SentimentChart />
       <OrdersChart />
       <MonthlyRevenueChart />
