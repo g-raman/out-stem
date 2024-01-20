88bea74beb1e09c2f06d0881cbaf72ba5802d7d2
Raman Gupta <gr.gupta.raman@gmail.com>
2024-01-20 17:48:29 -0500

    Change svg path

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -2,7 +2,7 @@ const NavBar = () => {
   return (
     <nav className="col-span-2 flex h-min items-center justify-center gap-4 rounded bg-background py-3 text-5xl font-bold">
       <span>
-        <img src="../../../public/out-stem.svg" height="50" width="50" />
+        <img src="/out-stem.svg" height="50" width="50" />
       </span>
       <span>Slice of Pi</span>
     </nav>
